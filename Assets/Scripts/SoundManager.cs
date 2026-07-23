cf7f11734e05dc895d9c57bfea8525162dd43867
SD115Regina <sd115regina@vfs.com>
2026-07-22 19:03:36 -0700

    added prefabs jdshfds

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -17,6 +17,16 @@ public class SoundManager : MonoBehaviour
     //NotSingleton
     [Header("Player Events")]
     public AK.Wwise.Event playerFire;
+    public AK.Wwise.Event weaponFireEvent;
+    public AK.Wwise.Event targetHitEvent;
+    public AK.Wwise.Event grenadeExplosionEvent;
+    public AK.Wwise.Event collectiblePickupEvent;
+    public AK.Wwise.Event paintballImpactEvent;
+    public AK.Wwise.Event uiClickEvent;
+    public AK.Wwise.Event targetSpawnEvent;
+    public AK.Wwise.Event sniperHitEvent;
+    public AK.Wwise.Event musicEvent;
+    public AK.Wwise.Event ambienceEvent;
 
     public void PlayerFire(GameObject gameObject)
     {
